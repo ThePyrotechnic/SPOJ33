c6d4959570e49b91398b250b41f8fbcf8b1b7783
Ed Minnix <egregius313@gmail.com>
2019-03-26 23:23:11 -0400

    Fixes

--- trip.py
+++ trip.py
@@ -35,38 +35,21 @@ def all_lcs_helper(table, alice, bob, a, b):
     if a == 0 or b == 0:
         return set()
     elif alice[a - 1] == bob[b - 1]:
-<<<<<<< HEAD
         # Can use alice or bob
         result = all_lcs_helper(table, alice, bob, a - 1, b - 1)
         if result:
             return {sub_lcs + alice[a - 1] for sub_lcs in result}
-=======
-        lcs = set()
-        res = all_lcs_helper(table, alice, bob, a - 1, b - 1)
-        if len(res) > 0:
-            for sub_lcs in res:
-                lcs.add(f'{sub_lcs}{alice[a - 1]}')
->>>>>>> 0ddb465f765ca50ca20852c1bc55ea3ea9a4330f
         else:
             return {alice[a - 1]}
     else:
         lcs = set()
         if table[a, b - 1] >= table[a - 1, b]:
-<<<<<<< HEAD
             result = all_lcs_helper(table, alice, bob, a, b - 1)
-            lcs_2.update(result)
+            lcs.update(result)
         if table[a - 1, b] >= table[a, b - 1]:
             result = all_lcs_helper(table, alice, bob, a - 1, b)
-            lcs_2.update(result)
-        return lcs_2
-=======
-            res = all_lcs_helper(table, alice, bob, a, b - 1)
-            lcs.update(res)
-        if table[a - 1, b] >= table[a, b - 1]:
-            res = all_lcs_helper(table, alice, bob, a - 1, b)
-            lcs.update(res)
+            lcs.update(result)
         return lcs
->>>>>>> 0ddb465f765ca50ca20852c1bc55ea3ea9a4330f
 
 
 def all_lcs(alice, bob):
